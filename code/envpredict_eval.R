97f61adec1cbcd944c5465939787b21f1781d62b
andand <doubleanders@gmail.com>
2025-05-23 13:52:18 +0200

    updated envpredict_eval.R

--- code/envpredict_eval.R
+++ code/envpredict_eval.R
@@ -1,7 +1,9 @@
 
 library(pheatmap)
+par(mgp = c(2.2, 1, 0))
 
-# physchem predictions from different taxonomic levels  - Random Forest
+##############################################################################
+### Physche#m predictions from different taxonomic levels  - Random Forest ###
 results_folder = "~/aquatic/envpredict/output/DifferentTaxonomicLevels"
 prediction_files = list.files(results_folder, pattern = "Predictions.tsv")
 prediction_files = prediction_files[c(grep("_16S_", prediction_files), grep("_18S_", prediction_files))]
@@ -31,9 +33,8 @@ barplot(colMeans(cor_matr), las = 2, ylab = "mean cor")
 pheatmap(cor_matr, cluster_cols = F, cluster_rows = F, labels_row = sample_labels)
 cor_matr_rf = cor_matr
 
-
-
-# physchem predictions from different taxonomic levels  - TabPNF
+#####################################################################
+### Physchem predictions from different taxonomic levels - TabPNF ###
 results_folder = "~/aquatic/envpredict/output/DifferentTaxonomicLevels_TabPNF"
 prediction_files = list.files(results_folder, pattern = "Predictions.tsv")
 prediction_files = prediction_files[c(grep("_16S_", prediction_files), grep("_18S_", prediction_files))]
@@ -73,7 +74,9 @@ wilcox.test(cor_matr_rf, cor_matr_tpnf, paired = T)
 plot(cor_matr_rf, cor_matr_tpnf, xlim = c(0.9, 1), ylim = c(0.9, 1))
 lines(c(0.8,1), c(0.8,1))
 
-# physchem predictions from RepresentationsFromDeepMicro
+
+##############################################################
+### Physchem predictions from RepresentationsFromDeepMicro ###
 results_folder = "~/aquatic/envpredict/output/RepresentationsFromDeepMicro"
 #results_folder = "~/aquatic/envpredict/output/RepresentationsFromDeepMicro_prefilt_features"
 prediction_files = list.files(results_folder, pattern = "Predictions.tsv")
@@ -107,46 +110,54 @@ pheatmap(cor_matr, cluster_cols = F, cluster_rows = F, labels_row = sample_label
 cor_matr_de = cor_matr
 #cor_matr_depf = cor_matr
 
-
-# physchem predictions from either 16S-ASVs or plankton microscopy
-results_folder = "~/aquatic/envpredict/output/physchem_based_on_seqdata_or_phytoplan"
-#results_folder = "~/aquatic/envpredict/output/physchem_based_on_seqdata_or_zooplan"
-prediction_files = list.files(results_folder, pattern = "Predictions.tsv")
-observation_files = gsub("Predictions", "Actual", prediction_files)
-pred_matr = read.delim(paste(results_folder, prediction_files[1], sep = "/"), row.names = 1)
-cor_matr = matrix(nrow = nrow(pred_matr), ncol = length(prediction_files))
-colnames(cor_matr) = gsub("_Predictions.tsv","", prediction_files)
-colnames(cor_matr) = gsub("-based_physchem","", colnames(cor_matr))
-rownames(cor_matr) = rownames(pred_matr)
-num_samples = c()
-for (i in 1:length(prediction_files)) {
-  pred_matr = as.matrix(read.delim(paste(results_folder, prediction_files[i], sep = "/"), row.names = 1))
-  obs_matr = as.matrix(read.delim(paste(results_folder, observation_files[i], sep = "/"), row.names = 1))
-  for (j in 1:nrow(pred_matr)) {
-    cor_matr[j,i] = cor.test(pred_matr[j,], obs_matr[j,])$est
-    #cor_matr[j,i] = (cor_matr[j,i])^2 # r-squared
-    cor_matr[j,i] = round(cor_matr[j,i], 3)
-    num_samples[j] = length(which(!is.na(obs_matr[j,])))
+########################################################################
+### Physchem predictions from either 16S-ASVs or plankton microscopy ###
+layout(matrix(c(1,1,2,3,4,4,5,6), 2, 4, byrow = F))
+results_folders = c() 
+results_folders[1] = "~/aquatic/envpredict/output/physchem_based_on_seqdata_or_phytoplan"
+results_folders[2] = "~/aquatic/envpredict/output/physchem_based_on_seqdata_or_zooplan"
+for (res_fol in 1:2) {
+  results_folder = results_folders[res_fol]
+  prediction_files = list.files(results_folder, pattern = "Predictions.tsv")
+  observation_files = gsub("Predictions", "Actual", prediction_files)
+  pred_matr = read.delim(paste(results_folder, prediction_files[1], sep = "/"), row.names = 1)
+  cor_matr = matrix(nrow = nrow(pred_matr), ncol = length(prediction_files))
+  colnames(cor_matr) = gsub("_Predictions.tsv","", prediction_files)
+  colnames(cor_matr) = gsub("-based_physchem","", colnames(cor_matr))
+  rownames(cor_matr) = rownames(pred_matr)
+  num_samples = c()
+  for (i in 1:length(prediction_files)) {
+    pred_matr = as.matrix(read.delim(paste(results_folder, prediction_files[i], sep = "/"), row.names = 1))
+    obs_matr = as.matrix(read.delim(paste(results_folder, observation_files[i], sep = "/"), row.names = 1))
+    for (j in 1:nrow(pred_matr)) {
+      cor_matr[j,i] = cor.test(pred_matr[j,], obs_matr[j,])$est
+      #cor_matr[j,i] = (cor_matr[j,i])^2 # r-squared
+      cor_matr[j,i] = round(cor_matr[j,i], 3)
+      num_samples[j] = length(which(!is.na(obs_matr[j,])))
+    }
   }
+  cor_matr
+  cor_matr = cor_matr[-c(1:7),]
+  num_samples = num_samples[-c(1:7)]
+  sample_labels = paste(rownames(cor_matr), " [", num_samples, "]", sep = "") 
+  #pheatmap(cor_matr, cluster_cols = F, cluster_rows = F, labels_row = sample_labels)
+  #layout(matrix(c(1,2,1,3), 2, 2, byrow = T))
+  par(mar = c(3,7,4,2), xpd = T)
+  #barplot(t(cor_matr[,]), beside = T, horiz = T, las = 1, legend = T, names.arg = sample_labels, cex.names = 0.8, args.legend = list(x = "top"))
+  barplot(t(cor_matr[,c(1,3)]), beside = T, horiz = T, las = 1, legend = T, names.arg = sample_labels, cex.names = 0.8, args.legend = list(x = "top", inset = c(-0.2,0)), xlab = "Pearson cor. (R)")
+  par(mar = c(6,2,2,2))
+  
+  #boxplot(cor_matr[,1], cor_matr[,2], cor_matr[,3],  names = colnames(cor_matr), las = 2, ylim = c(0.2, 1))
+  boxplot(cor_matr[,1], cor_matr[,3],  names = colnames(cor_matr)[c(1,3)], las = 2, ylim = c(0.2, 1))
+  par(mar = c(4,2,2,2))
+  plot(cor_matr[,1], cor_matr[,3], xlim = c(0,1), ylim = c(0,1), xlab = colnames(cor_matr)[1], ylab = colnames(cor_matr)[3])
+  lines(c(0,1), c(0,1))
+  wilcox.test(cor_matr[,1], cor_matr[,2], paired = T)
+  wilcox.test(cor_matr[,1], cor_matr[,3], paired = T)
 }
-cor_matr
-cor_matr = cor_matr[-c(1:7),]
-num_samples = num_samples[-c(1:7)]
-sample_labels = paste(rownames(cor_matr), " [", num_samples, "]", sep = "") 
-pheatmap(cor_matr, cluster_cols = F, cluster_rows = F, labels_row = sample_labels)
 
-layout(matrix(c(1,2,1,3), 2, 2, byrow = T)) 
-par(mar = c(2,10,2,2), xpd = T)
-barplot(t(cor_matr), beside = T, horiz = T, las = 1, legend = T, names.arg = sample_labels, cex.names = 0.8, args.legend = list(x = "top"))
-par(mar = c(6,4,2,2))
-boxplot(cor_matr[,1], cor_matr[,2], cor_matr[,3],  names = colnames(cor_matr), las = 2)
-par(mar = c(4,4,2,2))
-plot(cor_matr[,1], cor_matr[,3], xlim = c(0,1), ylim = c(0,1), xlab = colnames(cor_matr)[1], ylab = colnames(cor_matr)[3])
-lines(c(0,1), c(0,1))
-
-
-
-# phytoplankton predictions from either 16S-ASVs (ML) 18S-ASVs (ML), 18S-seq-matching, or physchem data
+#############################################################################################################
+### Phytoplankton predictions from either 16S-ASVs (ML) 18S-ASVs (ML), 18S-seq-matching, or physchem data ###
 results_folder = "~/aquatic/envpredict/output/phytoplankton_predicted"
 prediction_files = list.files(results_folder, pattern = "Predictions.tsv")
 prediction_files = prediction_files[c(grep("phyt_plan_genus", prediction_files))] # grep("direct_matching_", prediction_files))]
@@ -160,7 +171,7 @@ for (i in 1:length(prediction_files)) {
 cor_matr = matrix(nrow = length(shared_genus), ncol = length(prediction_files))
 colnames(cor_matr) = gsub("_Predictions.tsv","", prediction_files)
 colnames(cor_matr) = gsub("phyt_plan_genus_","", colnames(cor_matr))
-colnames(cor_matr) = gsub("direct_matching_norm_clade_counts_18S_8_phyt_plan_genus","matching_18S", colnames(cor_matr))
+colnames(cor_matr) = gsub("direct_matching_norm_clade_counts_18S_8_phyt_plan_genus","match_18S", colnames(cor_matr))
 colnames(cor_matr) = gsub("norm_asv_counts_","ML_", colnames(cor_matr))
 colnames(cor_matr) = gsub("_RF10fold","", colnames(cor_matr))
 colnames(cor_matr) = gsub("renomralized_matching_18","renorm_matching_18", colnames(cor_matr))
@@ -182,6 +193,8 @@ for (i in 1:length(prediction_files)) {
 ix = which(rowSums(is.na(cor_matr)) == 0)
 cor_matr = cor_matr[ix,]
 num_samples = num_samples[ix]
+cor_matr_phytoplan = cor_matr
+num_samples_phytoplan = num_samples
 sample_labels = paste(rownames(cor_matr), " [", num_samples, "]", sep = "") 
 par(mar = c(2,10,2,2))
 barplot(t(cor_matr), beside = T, horiz = T, las = 1, legend = T, names.arg = sample_labels, cex.names = 0.8, args.legend = list(x = "topright"))
@@ -191,7 +204,7 @@ layout(matrix(c(1,2,5,3,4,6), 2, 3, byrow = T))
 ix2 = c(4,2,3,1)
 par(mar = c(4,4,1,1))
 for (i in 1:length(ix2)) {
-  plot(num_samples, cor_matr[,ix2[i]], main = colnames(cor_matr)[ix2[i]], xlab = "Detected in num samples", ylab = "Pearson cor (R)")
+  plot(num_samples, cor_matr[,ix2[i]], main = colnames(cor_matr)[ix2[i]], ylim = c(0,1), xlab = "Detected in num samples", ylab = "Pearson cor (R)")
 }
 ix = which(num_samples > 50)
 par(mar = c(6,4,1,1))
@@ -202,11 +215,11 @@ boxplot(
 par(mar = c(4,4,1,1))
 plot(cor_matr[ix,ix2[4]], cor_matr[ix,ix2[3]], xlim = c(0,1), ylim = c(0,1), xlab = colnames(cor_matr)[ix2[4]], ylab = colnames(cor_matr)[ix2[3]])
 lines(c(0,1), c(0,1))
+ix = which(num_samples > 50)
+wilcox.test(cor_matr[ix,ix2[1]], cor_matr[ix,ix2[2]], paired = T)
+wilcox.test(cor_matr[ix,ix2[1]], cor_matr[ix,ix2[3]], paired = T)
 
-
-
-
-# zooplankton predictions from either 16S-ASVs (ML) 18S-ASVs (ML), 18S-seq-matching, or physiochem data
+## Zooplankton predictions from either 16S-ASVs (ML) 18S-ASVs (ML), 18S-seq-matching, or physiochem data
 results_folder = "~/aquatic/envpredict/output/zooplankton_predicted"
 prediction_files = list.files(results_folder, pattern = "Predictions.tsv")
 prediction_files = prediction_files[c(grep("zoo_plan_genus", prediction_files))] # grep("direct_matching_", prediction_files))]
@@ -220,7 +233,7 @@ for (i in 1:length(prediction_files)) {
 cor_matr = matrix(nrow = length(shared_genus), ncol = length(prediction_files))
 colnames(cor_matr) = gsub("_Predictions.tsv","", prediction_files)
 colnames(cor_matr) = gsub("zoo_plan_genus_","", colnames(cor_matr))
-colnames(cor_matr) = gsub("direct_matching_norm_clade_counts_18S_8_zoo_plan_genus","matching_18S", colnames(cor_matr))
+colnames(cor_matr) = gsub("direct_matching_norm_clade_counts_18S_8_zoo_plan_genus","match_18S", colnames(cor_matr))
 colnames(cor_matr) = gsub("norm_asv_counts_","ML_", colnames(cor_matr))
 colnames(cor_matr) = gsub("_RF10fold","", colnames(cor_matr))
 colnames(cor_matr) = gsub("renomralized_matching_18","renorm_matching_18", colnames(cor_matr))
@@ -242,17 +255,17 @@ for (i in 1:length(prediction_files)) {
 ix = which(rowSums(is.na(cor_matr)) == 0)
 cor_matr = cor_matr[ix,]
 num_samples = num_samples[ix]
+cor_matr_zooplan = cor_matr
+num_samples_zooplan = num_samples
 sample_labels = paste(rownames(cor_matr), " [", num_samples, "]", sep = "") 
-
 par(mfrow = c(1,1), mar = c(2,10,2,2))
 barplot(t(cor_matr), beside = T, horiz = T, las = 1, legend = T, names.arg = sample_labels, cex.names = 0.8, args.legend = list(x = "topright"))
 pheatmap(cor_matr, cluster_cols = F, cluster_rows = F, labels_row = sample_labels)
-# plotting cor vs number of micr samples where genus was observed
 layout(matrix(c(1,2,5,3,4,6), 2, 3, byrow = T))
 ix2 = c(4,2,3,1)
 par(mar = c(4,4,1,1))
 for (i in 1:length(ix2)) {
-  plot(num_samples, cor_matr[,ix2[i]], main = colnames(cor_matr)[ix2[i]], xlab = "Detected in num samples", ylab = "Pearson cor (R)")
+  plot(num_samples, cor_matr[,ix2[i]], main = colnames(cor_matr)[ix2[i]], ylim = c(0,1), xlab = "Detected in num samples", ylab = "Pearson cor (R)")
 }
 ix = which(num_samples > 50)
 par(mar = c(6,4,1,1))
@@ -263,34 +276,46 @@ boxplot(
 par(mar = c(4,4,1,1))
 plot(cor_matr[ix,ix2[4]], cor_matr[ix,ix2[3]], xlim = c(0,1), ylim = c(0,1), xlab = colnames(cor_matr)[ix2[4]], ylab = colnames(cor_matr)[ix2[3]])
 lines(c(0,1), c(0,1))
-
-
-
-
-
-par(mfrow = c(2,2), mar = c(4,4,1,1))
-for (i in 1:length(ix2)) {
-  plot(num_samples, cor_matr[,ix2[i]], main = colnames(cor_matr)[ix2[i]], xlab = "Detected in num samples", ylab = "Pearson cor (R)")
-}
-# boxplots of cor per method, including genera observed in at least of 50 samples
 ix = which(num_samples > 50)
+wilcox.test(cor_matr[ix,ix2[1]], cor_matr[ix,ix2[2]], paired = T)
+wilcox.test(cor_matr[ix,ix2[1]], cor_matr[ix,ix2[3]], paired = T)
+
+# plotting the phyto and zooplankton predictions in one figure (requires the above 2 sections to be run first)
+layout(matrix(c(1,2,5,3,4,6,7,8,11,9,10,12), 4, 3, byrow = T))
 ix2 = c(4,2,3,1)
-par(mfrow = c(1,1), mar = c(10,4,1,1))
+num_samples = num_samples_phytoplan
+cor_matr = cor_matr_phytoplan
+par(mar = c(5,4,1,1), mgp = c(2,1,0))
+for (i in 1:length(ix2)) {
+  plot(num_samples, cor_matr[,ix2[i]], main = colnames(cor_matr)[ix2[i]], ylim = c(0,1), xlab = "Detected in num samples", ylab = "Pearson cor (R)")
+}
+ix = which(num_samples > 50)
 boxplot(
   cor_matr[ix,ix2[1]], cor_matr[ix,ix2[2]], cor_matr[ix,ix2[3]], cor_matr[ix,ix2[4]], 
-  names = colnames(cor_matr)[ix2], las = 3, ylab = "Pearson cor (R)"
+  horizontal = F, names = colnames(cor_matr)[ix2], las = 3, ylab = "Pearson cor (R)"
 )
-wilcox.test(cor_matr[,"phys_chem"], cor_matr[,"ML_16S"], paired = T)
-wilcox.test(cor_matr[,"phys_chem"], cor_matr[,"ML_18S"], paired = T)
-plot(cor_matr[ix,ix2[1]], cor_matr[ix,ix2[3]], xlim = c(0,1), ylim = c(0,1), xlab = colnames(cor_matr)[ix2[1]], ylab = colnames(cor_matr)[ix2[3]])
-lines(c(0,1), c(0,1))
+par(mar = c(4,4,1,1))
 plot(cor_matr[ix,ix2[4]], cor_matr[ix,ix2[3]], xlim = c(0,1), ylim = c(0,1), xlab = colnames(cor_matr)[ix2[4]], ylab = colnames(cor_matr)[ix2[3]])
 lines(c(0,1), c(0,1))
-plot(cor_matr[ix,ix2[2]], cor_matr[ix,ix2[3]], xlim = c(0,1), ylim = c(0,1), xlab = colnames(cor_matr)[ix2[2]], ylab = colnames(cor_matr)[ix2[3]])
+
+num_samples = num_samples_zooplan
+cor_matr = cor_matr_zooplan
+par(mar = c(5,4,1,1), mgp = c(2,1,0))
+for (i in 1:length(ix2)) {
+  plot(num_samples, cor_matr[,ix2[i]], main = colnames(cor_matr)[ix2[i]], ylim = c(0,1), xlab = "Detected in num samples", ylab = "Pearson cor (R)")
+}
+ix = which(num_samples > 50)
+boxplot(
+  cor_matr[ix,ix2[1]], cor_matr[ix,ix2[2]], cor_matr[ix,ix2[3]], cor_matr[ix,ix2[4]], 
+  horizontal = F, names = colnames(cor_matr)[ix2], las = 3, ylab = "Pearson cor (R)"
+)
+par(mar = c(4,4,1,1))
+plot(cor_matr[ix,ix2[4]], cor_matr[ix,ix2[3]], xlim = c(0,1), ylim = c(0,1), xlab = colnames(cor_matr)[ix2[4]], ylab = colnames(cor_matr)[ix2[3]])
 lines(c(0,1), c(0,1))
 
 
-## phytoplankton-genus and 18S-genus predictions based on physchem data
+############################################################################
+### Phytoplankton-genus and 18S-genus predictions based on physchem data ###
 results_folder = "~/aquatic/envpredict/output/phytoplankton_genus_micr_and_18S_based_on_physchem"
 prediction_files = list.files(results_folder, pattern = "Predictions.tsv")
 observation_files = gsub("Predictions", "Actual", prediction_files)
